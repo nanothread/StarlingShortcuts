26c9f9f1f2b4a1213e3cb5df8d6bbfc67e7456ee
Andrew Glen <andrew@glenagon.com>
2020-09-28 22:03:08 +0100

    Initial Commit

---
+++ Starling Shortcuts/Starling_ShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starling_ShortcutsApp.swift
+//  Starling Shortcuts
+//
+//  Created by Andrew Glen on 28/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Starling_ShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
